aaae00f13122f21cd0a1fcac5403d958140180f3
Oleg Chernovskiy <adonai@xaker.ru>
2017-11-05 23:13:54 +0300

    Basic ui

--- app/src/main/java/dybr/kanedias/com/fair/MainActivity.kt
+++ app/src/main/java/dybr/kanedias/com/fair/MainActivity.kt
@@ -1,25 +1,17 @@
 package dybr.kanedias.com.fair
 
-import android.animation.Animator
-import android.animation.AnimatorListenerAdapter
-import android.animation.ValueAnimator
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.support.design.widget.TabLayout
 import android.support.v4.view.GravityCompat
-import android.support.v4.view.animation.FastOutSlowInInterpolator
 import android.support.v4.widget.DrawerLayout
 import android.support.v7.widget.Toolbar
-import android.view.Gravity
 import android.view.Menu
 import android.view.MenuInflater
-import android.view.View
-import android.widget.ImageView
-import android.widget.LinearLayout
-import android.widget.RelativeLayout
+import android.widget.*
 import butterknife.BindView
 import butterknife.ButterKnife
-import butterknife.OnClick
+import dybr.kanedias.com.fair.ui.Sidebar
 
 /**
  * Main activity with drawer and sliding tabs where most of user interaction happens.
@@ -48,22 +40,15 @@ class MainActivity : AppCompatActivity() {
     lateinit var tabs: TabLayout
 
     /**
-     * Sidebar header
+     * Top-level sidebar content list view
      */
-    @BindView(R.id.back)
-    lateinit var sidebarHeader: RelativeLayout
+    @BindView(R.id.sidebar_content)
+    lateinit var sidebarContent: ListView
 
     /**
-     * Sidebar accounts area (bottom of header)
+     * Sidebar that slides from the left (the second part of drawer)
      */
-    @BindView(R.id.accounts_area)
-    lateinit var accountsArea: LinearLayout
-
-    /**
-     * Sidebar header up/down image (to the right of welcome text)
-     */
-    @BindView(R.id.header_flip)
-    lateinit var headerFlip: ImageView
+    private lateinit var sidebar: Sidebar
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -75,19 +60,15 @@ class MainActivity : AppCompatActivity() {
         setupUI()
     }
 
-    @OnClick(R.id.back)
-    private fun toggleHeader() {
-        if (accountsArea.visibility == View.GONE) {
-            expand(accountsArea)
-            flipAnimator(false, headerFlip).start()
-        } else {
-            collapse(accountsArea)
-            flipAnimator(true, headerFlip).start()
-        }
-    }
-
     private fun setupUI() {
+        var header = layoutInflater.inflate(R.layout.activity_main_sidebar_header, sidebarContent, false)
 
+        sidebarContent.dividerHeight = 0
+        sidebarContent.descendantFocusability = ListView.FOCUS_BEFORE_DESCENDANTS
+        sidebarContent.addHeaderView(header)
+        sidebarContent.adapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, mutableListOf(1, 2, 3))
+
+        sidebar = Sidebar(this)
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
@@ -104,70 +85,5 @@ class MainActivity : AppCompatActivity() {
         }
 
         super.onBackPressed()
-    }
-
-    /**
-     * Returns created animator.
-     * Animates via slowly negating scaleY of target view
-     */
-    private fun flipAnimator(isFlipped: Boolean, v: View): ValueAnimator {
-        val animator = ValueAnimator.ofFloat(if (isFlipped) -1f else 1f, if (isFlipped) 1f else -1f)
-        animator.interpolator = FastOutSlowInInterpolator()
-
-        animator.addUpdateListener { valueAnimator ->
-            // update view height when flipping
-            v.scaleY = valueAnimator.animatedValue as Float
-        }
-        return animator
-    }
-
-    /**
-     * Returns created animator.
-     * Animates via slowly changing target view height
-     */
-    private fun slideAnimator(start: Int, end: Int, v: View): ValueAnimator {
-        val animator = ValueAnimator.ofInt(start, end)
-        animator.interpolator = FastOutSlowInInterpolator()
-
-        animator.addUpdateListener { valueAnimator ->
-            // update height
-            val value = valueAnimator.animatedValue as Int
-            val layoutParams = v.layoutParams
-            layoutParams.height = value
-            v.layoutParams = layoutParams
-        }
-        return animator
-    }
-
-    /**
-     * Expands target layout by making it visible and increasing its height
-     * @see slideAnimator
-     */
-    private fun expand(v: LinearLayout) {
-        // set layout visible
-        v.visibility = View.VISIBLE
-
-        val widthSpec = View.MeasureSpec.makeMeasureSpec(0, View.MeasureSpec.UNSPECIFIED)
-        val heightSpec = View.MeasureSpec.makeMeasureSpec(0, View.MeasureSpec.UNSPECIFIED)
-        v.measure(widthSpec, heightSpec)
-
-        val animator = slideAnimator(0, v.measuredHeight, v)
-        animator.start()
-    }
-
-    /**
-     * Collapses target layout by decreasing its height and making it gone
-     * @see slideAnimator
-     */
-    private fun collapse(v: LinearLayout) {
-        val finalHeight = v.height
-        val animator = slideAnimator(finalHeight, 0, v)
-
-        animator.addListener(object : AnimatorListenerAdapter() {
-            override fun onAnimationEnd(animator: Animator) {
-                v.visibility = View.GONE
-            }
-        })
-        animator.start()
     }
 }
---
+++ app/src/main/java/dybr/kanedias/com/fair/ui/Sidebar.kt
@@ -0,0 +1,121 @@
+package dybr.kanedias.com.fair.ui
+
+import android.animation.Animator
+import android.animation.AnimatorListenerAdapter
+import android.animation.ValueAnimator
+import android.support.v4.view.animation.FastOutSlowInInterpolator
+import android.support.v7.app.AppCompatActivity
+import android.view.View
+import android.widget.ImageView
+import android.widget.LinearLayout
+import android.widget.RelativeLayout
+import butterknife.BindView
+import butterknife.ButterKnife
+import butterknife.OnClick
+import dybr.kanedias.com.fair.R
+
+/**
+ * @author Kanedias
+ *
+ * Created on 05.11.17
+ */
+class Sidebar(parent: AppCompatActivity) {
+
+    init {
+        ButterKnife.bind(this, parent)
+    }
+
+    /**
+     * Sidebar header
+     */
+    @BindView(R.id.sidebar_header_area)
+    lateinit var sidebarHeader: RelativeLayout
+
+    /**
+     * Sidebar accounts area (bottom of header)
+     */
+    @BindView(R.id.accounts_area)
+    lateinit var accountsArea: LinearLayout
+
+    /**
+     * Sidebar header up/down image (to the right of welcome text)
+     */
+    @BindView(R.id.header_flip)
+    lateinit var headerFlip: ImageView
+
+    @OnClick(R.id.sidebar_header_area)
+    fun toggleHeader() {
+        if (accountsArea.visibility == View.GONE) {
+            expand(accountsArea)
+            flipAnimator(false, headerFlip).start()
+        } else {
+            collapse(accountsArea)
+            flipAnimator(true, headerFlip).start()
+        }
+    }
+
+    /**
+     * Returns created animator.
+     * Animates via slowly negating scaleY of target view
+     */
+    private fun flipAnimator(isFlipped: Boolean, v: View): ValueAnimator {
+        val animator = ValueAnimator.ofFloat(if (isFlipped) -1f else 1f, if (isFlipped) 1f else -1f)
+        animator.interpolator = FastOutSlowInInterpolator()
+
+        animator.addUpdateListener { valueAnimator ->
+            // update view height when flipping
+            v.scaleY = valueAnimator.animatedValue as Float
+        }
+        return animator
+    }
+
+    /**
+     * Returns created animator.
+     * Animates via slowly changing target view height
+     */
+    private fun slideAnimator(start: Int, end: Int, v: View): ValueAnimator {
+        val animator = ValueAnimator.ofInt(start, end)
+        animator.interpolator = FastOutSlowInInterpolator()
+
+        animator.addUpdateListener { valueAnimator ->
+            // update height
+            val value = valueAnimator.animatedValue as Int
+            val layoutParams = v.layoutParams
+            layoutParams.height = value
+            v.layoutParams = layoutParams
+        }
+        return animator
+    }
+
+    /**
+     * Expands target layout by making it visible and increasing its height
+     * @see slideAnimator
+     */
+    private fun expand(v: LinearLayout) {
+        // set layout visible
+        v.visibility = View.VISIBLE
+
+        val widthSpec = View.MeasureSpec.makeMeasureSpec(0, View.MeasureSpec.UNSPECIFIED)
+        val heightSpec = View.MeasureSpec.makeMeasureSpec(0, View.MeasureSpec.UNSPECIFIED)
+        v.measure(widthSpec, heightSpec)
+
+        val animator = slideAnimator(0, v.measuredHeight, v)
+        animator.start()
+    }
+
+    /**
+     * Collapses target layout by decreasing its height and making it gone
+     * @see slideAnimator
+     */
+    private fun collapse(v: LinearLayout) {
+        val finalHeight = v.height
+        val animator = slideAnimator(finalHeight, 0, v)
+
+        animator.addListener(object : AnimatorListenerAdapter() {
+            override fun onAnimationEnd(animator: Animator) {
+                v.visibility = View.GONE
+            }
+        })
+        animator.start()
+    }
+}
