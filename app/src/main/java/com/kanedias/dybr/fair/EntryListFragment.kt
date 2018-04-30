c9956eeb340ec3f94cc58431a06e1e9f7948014c
Oleg Chernovskiy <adonai@xaker.ru>
2018-05-01 00:44:07 +0300

    Implement paging support

--- app/src/main/java/com/kanedias/dybr/fair/EntryListFragment.kt
+++ app/src/main/java/com/kanedias/dybr/fair/EntryListFragment.kt
@@ -13,6 +13,8 @@ import android.view.ViewGroup
 import butterknife.BindView
 import butterknife.ButterKnife
 import com.kanedias.dybr.fair.entities.*
+import com.squareup.moshi.Moshi
+import com.squareup.moshi.Types
 import kotlinx.coroutines.experimental.*
 import kotlinx.coroutines.experimental.android.UI
 import moe.banana.jsonapi2.ArrayDocument
@@ -36,6 +38,8 @@ class EntryListFragment: Fragment() {
     var blog: Blog? = null
 
     private val entryAdapter = EntryListAdapter()
+    private var nextPage = 1
+    private var lastPage = false
 
     private lateinit var activity: MainActivity
 
@@ -50,8 +54,9 @@ class EntryListFragment: Fragment() {
     }
 
     private fun setupUI() {
-        refresher.setOnRefreshListener { refreshEntries() }
+        refresher.setOnRefreshListener { refreshEntries(true) }
         entryRibbon.layoutManager = LinearLayoutManager(activity)
+        entryRibbon.adapter = entryAdapter
     }
 
     /**
@@ -72,22 +77,30 @@ class EntryListFragment: Fragment() {
         }
     }
 
-    fun refreshEntries() {
+    /**
+     * Loads the next page in entry listing. If no pages were loaded before, loads first
+     * @param reset reset page counter to first
+     */
+    fun refreshEntries(reset: Boolean = false) {
         if (blog == null) // we don't have a blog, just show empty list
             return
+
+        if (reset) {
+            nextPage = 1
+            lastPage = false
+        }
 
         launch(UI) {
             refresher.isRefreshing = true
 
             try {
-                val success = async(CommonPool) { Network.loadEntries(blog!!) }
-                entryAdapter.entries = success.await()
+                val success = async(CommonPool) { Network.loadEntries(blog!!, nextPage) }
+                updateRibbonPage(success.await(), reset)
             } catch (ex: Exception) {
                 Network.reportErrors(activity, ex)
             }
 
             refresher.isRefreshing = false
-            entryRibbon.adapter = entryAdapter
 
             if (blog == Auth.blog) {
                 activity.actionButton.show()
@@ -95,23 +108,95 @@ class EntryListFragment: Fragment() {
         }
     }
 
+    /**
+     * Update entry ribbon with newly loaded values.
+     * @param loaded document with entries and links to pages that was loaded
+     * @param reset if true, clear current entries before populating from [loaded]
+     */
+    private fun updateRibbonPage(loaded: ArrayDocument<Entry>, reset: Boolean) {
+        val linkMap = linksToMap(loaded)
+        if (!linkMap.containsKey("next")) {
+            lastPage = true
+        }
+        nextPage += 1
+
+        entryAdapter.apply {
+            if (reset) {
+                entries.clear()
+            }
+            entries.addAll(loaded)
+            notifyDataSetChanged()
+        }
+    }
+
+    /**
+     * Converts links object from document to map string<->string
+     * @param loaded Json-API document containing `links {}` part
+     */
+    @Suppress("UNCHECKED_CAST")
+    private fun linksToMap(loaded: ArrayDocument<Entry>): Map<String, String> {
+        val mapType = Types.newParameterizedType(Map::class.java, String::class.java, String::class.java)
+        val mapAdapter = Moshi.Builder().build().adapter<Map<String, String>>(mapType)
+        return loaded.links.get<Map<String, String>>(mapAdapter) as Map<String, String>
+    }
+
     inner class EntryListAdapter : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
 
-        var entries: List<Entry> = ArrayList()
+        private val REGULAR_POST = 0
+        private val LOAD_MORE = 1
+
+        var entries: MutableList<Entry> = ArrayList()
+
+        override fun getItemViewType(position: Int): Int {
+            if (position < entries.size) {
+                return REGULAR_POST
+            }
+
+            return LOAD_MORE
+        }
 
         override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
-            val entryHolder = holder as EntryViewHolder
-            val entry = entries[position]
-            entryHolder.setup(entry, blog == Auth.blog)
+            when (getItemViewType(position)) {
+                REGULAR_POST -> {
+                    val entryHolder = holder as EntryViewHolder
+                    val entry = entries[position]
+                    entryHolder.setup(entry, blog == Auth.blog)
+                }
+                else -> refreshEntries()
+            }
+
         }
 
         override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
             val inflater = LayoutInflater.from(activity)
-            val view = inflater.inflate(R.layout.fragment_entry_list_item, parent, false)
-            return EntryViewHolder(view)
+            return when (viewType) {
+                REGULAR_POST -> {
+                    val view = inflater.inflate(R.layout.fragment_entry_list_item, parent, false)
+                    EntryViewHolder(view)
+                }
+                else -> {
+                    val pbar = inflater.inflate(R.layout.view_load_more, parent, false)
+                    object: RecyclerView.ViewHolder(pbar) {}
+                }
+            }
         }
 
-        override fun getItemCount() = entries.size
+        override fun getItemCount() : Int {
+            if (entries.isEmpty()) {
+                // seems like we didn't yet load anything in our view, it's probably loading
+                // for the first time. Don't show "load more", let's wait while anything shows up.
+                return 0
+            }
+
+            // we have something in the view already
+            var size = entries.size
+
+            // show "load more" holder if it's not the last page
+            if (!lastPage)
+                size += 1
+
+            return size
+        }
     }
 
     /**
