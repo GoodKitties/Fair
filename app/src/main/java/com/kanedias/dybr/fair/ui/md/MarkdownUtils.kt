36f0acab60da06eaf70071201a53cfd3f4095fe2
Oleg Chernovskiy <adonai@xaker.ru>
2018-06-03 21:59:51 +0300

    Groundwork, first working MORE and img-on-demand system
    - no tests though!

--- app/src/main/java/com/kanedias/dybr/fair/ui/md/MarkdownUtils.kt
+++ app/src/main/java/com/kanedias/dybr/fair/ui/md/MarkdownUtils.kt
@@ -1,12 +1,21 @@
 package com.kanedias.dybr.fair.ui.md
 
+import android.content.Context
 import android.graphics.Bitmap
 import android.graphics.BitmapFactory
 import android.graphics.Rect
 import android.graphics.drawable.BitmapDrawable
 import android.graphics.drawable.Drawable
+import android.text.SpannableStringBuilder
+import android.text.Spanned
+import android.text.TextUtils
+import android.text.style.CharacterStyle
+import android.text.style.ClickableSpan
+import android.text.style.ImageSpan
+import android.view.View
 import android.widget.TextView
 import com.kanedias.dybr.fair.Network
+import com.kanedias.dybr.fair.R
 import com.kanedias.dybr.fair.misc.CustomTextView
 import com.kanedias.html2md.Html2Markdown
 import kotlinx.coroutines.experimental.*
@@ -14,11 +23,15 @@ import kotlinx.coroutines.experimental.android.UI
 import okhttp3.MediaType
 import okhttp3.Request
 import okhttp3.Response
+import org.w3c.dom.Text
 import pl.droidsonroids.gif.GifDrawable
 import pl.droidsonroids.gif.GifDrawableBuilder
 import ru.noties.markwon.Markwon
 import ru.noties.markwon.SpannableConfiguration
 import ru.noties.markwon.spans.AsyncDrawable
+import ru.noties.markwon.spans.AsyncDrawableSpan
+import ru.noties.markwon.spans.LinkSpan
+import ru.noties.markwon.spans.SpannableTheme
 import java.io.IOException
 import kotlin.math.ceil
 
@@ -33,8 +46,102 @@ infix fun TextView.handleMarkdown(html: String) {
     val mdConfig = SpannableConfiguration.builder(this.context)
             .asyncDrawableLoader(DrawableLoader(this))
             .build()
-    Markwon.setMarkdown(this, mdConfig, Html2Markdown().parse(html))
+    val spanned = Markwon.markdown(mdConfig, Html2Markdown().parse(html)) as SpannableStringBuilder
+    postProcessSpans(this, spanned)
+
+    this.text = spanned
     this.movementMethod = CustomTextView.LocalLinkMovementMethod()
+}
+
+/**
+ * Post-process spans like MORE or image loading
+ */
+fun postProcessSpans(view: TextView, spanned: SpannableStringBuilder) {
+    postProcessDrawables(spanned, view)
+    postProcessMore(spanned, view)
+
+}
+
+/**
+ * Post-process MORE statements in the text. They act like `<spoiler>` or `<cut>` tag in some websites
+ * @param spanned text to be modified to cut out MORE tags and insert replacements instead of them
+ * @param view resulting text view to accept the modified spanned string
+ */
+fun postProcessMore(spanned: SpannableStringBuilder, view: TextView) {
+    while (true) {
+        // we need to process all MOREs in the text, start from inner ones, get back to outer in next loops
+        val moreDetector = Regex(".*(\\[MORE=(.+?)](.*?)\\[\\/MORE])", RegexOption.DOT_MATCHES_ALL)
+        val match = moreDetector.find(spanned) ?: break
+        // we have a match, make a replacement
+
+        // get group content out of regex
+        val outerRange = match.groups[1]!!.range // from start of [MORE] to the end of [/MORE]
+        val moreText = match.groups[2]!!.value // content inside opening tag [MORE=...]
+        val innerRange = match.groups[3]!!.range // range between opening and closing tag of MORE
+        val innerText = match.groups[3]!!.value // content between opening and closing tag of MORE
+        val innerSpanned = spanned.subSequence(innerRange.start, innerRange.start + innerText.length) // contains all spans there
+
+        spanned.replace(outerRange.start, outerRange.endInclusive + 1, moreText) // replace it just with text
+        val wrapper = object : ClickableSpan() {
+
+            override fun onClick(widget: View?) {
+                // replace wrappers with real previous spans
+
+                val start = spanned.getSpanStart(this)
+                val end = spanned.getSpanEnd(this)
+                spanned.removeSpan(this)
+                spanned.replace(start, end, innerSpanned)
+
+                view.text = spanned
+                Markwon.scheduleDrawables(view)
+            }
+        }
+        spanned.setSpan(wrapper, outerRange.start, outerRange.start + moreText.length, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE)
+    }
+
+    view.text = spanned
+    Markwon.scheduleDrawables(view)
+}
+
+/**
+ * Post-process async drawables in the post. We have to do this because lots of drawables can overload native memory
+ * reserved for the application.
+ *
+ * We replace such drawables with placeholders so if you need to actually load them you can
+ *
+ * @param spanned text to be modified. We cut out async drawables and put image placeholders instead of them there
+ * @param view text view to accept resulting spanned string. On placeholder click show wrapped async drawables
+ */
+private fun postProcessDrawables(spanned: SpannableStringBuilder, view: TextView) {
+    val spans = spanned.getSpans(0, spanned.length, AsyncDrawableSpan::class.java)
+    for (span in spans) {
+        val start = spanned.getSpanStart(span)
+        val end = spanned.getSpanEnd(span)
+        val spansToWrap = spanned.getSpans(start, end, CharacterStyle::class.java)
+        val wrapperImg = ImageSpan(view.context, R.drawable.cloud_download)
+        val wrapperClick = object: ClickableSpan() {
+
+            override fun onClick(widget: View?) {
+                // replace wrappers with real previous spans
+
+                // text can be already moved around (due to MORE etc.), refresh start/end variables
+                val realStart = spanned.getSpanStart(this)
+                val realEnd = spanned.getSpanEnd(this)
+
+                spanned.removeSpan(wrapperImg)
+                spanned.removeSpan(this)
+
+                spansToWrap.forEach { spanned.setSpan(it, realStart, realEnd, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE) }
+                view.text = spanned
+                Markwon.scheduleDrawables(view)
+            }
+
+        }
+
+        spansToWrap.forEach { spanned.removeSpan(it) }
+        spanned.setSpan(wrapperImg, start, end, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE)
+        spanned.setSpan(wrapperClick, start, end, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE)
+    }
 }
 
 /**
--- app/src/main/java/com/kanedias/dybr/fair/ui/md/MdImgExtension.kt
+++
@@ -1,7 +0,0 @@
-package com.kanedias.dybr.fair.ui.md
-
-/**
- * @author Kanedias
- *
- * Created on 01.05.18
- */
